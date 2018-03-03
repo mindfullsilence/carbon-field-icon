5765db1fc0bc466257aa346d30fbdcbbf7f834eb
Timothy Anderson <mindfullsilence@gmail.com>
2018-03-02 17:22:59 -0700

    wrap in static class

--- field.php
+++ field.php
@@ -7,8 +7,15 @@ use Carbon_Fields\Carbon_Fields;
 define( 'Carbon_Field_Icon\\VERSION', '2.0.0' );
 define( 'Carbon_Field_Icon\\DIR', __DIR__ );
 
-add_action('init', function () {
-	Carbon_Fields::extend( Icon_Field::class, function( $container ) {
-		return new Icon_Field( $container['arguments']['type'], $container['arguments']['name'], $container['arguments']['label'] );
-	} );
-});
+
+class Field_Loader {
+
+	public static function init() {
+
+		Carbon_Fields::extend( Icon_Field::class, function ( $container ) {
+			return new Icon_Field( $container['arguments']['type'], $container['arguments']['name'], $container['arguments']['label'] );
+		} );
+
+	}
+
+}
